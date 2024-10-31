23ec5ce62cb29ed83f93f779d5a6f1a31d646edf
Kunjung <kunjungsherpa@gmail.com>
2024-10-31 19:02:48 +0545

    added authentication permissions

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
     path('booking/', views.BookingView.as_view()),
     path('booking/<int:pk>', views.SingleBookingView.as_view()),
+
+    path('message/', views.msg),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
@@ -24,4 +27,8 @@ class SingleBookingView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView)
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
+@api_view()
+@permission_classes([IsAuthenticated])
+def msg(request):
+    return Response({"message": "This view is secret."})
 
