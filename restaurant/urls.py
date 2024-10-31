f36f404be465d5708033165c3a9b0dbcc69eb119
Kunjung <kunjungsherpa@gmail.com>
2024-10-31 18:47:50 +0545

    views added

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('booking/', views.BookingView.as_view()),
+    path('booking/<int:pk>', views.SingleBookingView.as_view()),
 ]
