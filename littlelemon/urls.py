9dbd224af4791a620b325702ab9c3af2bdca95a0
Kunjung <kunjungsherpa@gmail.com>
2024-10-31 19:25:28 +0545

    added readme

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from restaurant.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='home'),
     path('api/', include('restaurant.urls')),
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='home'),
-    
+urlpatterns = [    
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
     path('booking/', views.BookingView.as_view()),
