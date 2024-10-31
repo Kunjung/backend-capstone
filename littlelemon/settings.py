3493748c418d30ec6108e4510bcdeeb3a22fb21b
Kunjung <kunjungsherpa@gmail.com>
2024-10-31 19:28:28 +0545

    use mysql connection

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    # 'sqldb': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'littlelemon',
-    #     'USER': 'root',
-    #     'PASSWORD': '',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '3006'
-    # }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3006'
+    }
 }
 
 
